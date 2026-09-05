755a30f96394a8c2f776cbe99c1e846e703b228a
thailisg <thai.ori.gon.diaz@gmail.com>
2026-09-05 18:01:50 -0400

    Establishing the correct connections with the database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req, res) => {res.send('Hello world');});
+//router.get('/', (req, res) => {res.send('Hello world');});
+
+router.use('/users', require('./users'));
 
 module.exports = router;
