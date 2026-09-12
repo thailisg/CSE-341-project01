f26f124e9f1d2b3d86d70d047cd0a5d668ab4407
thailisg <thai.ori.gon.diaz@gmail.com>
2026-09-12 18:12:59 -0300

    documentation

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 const usersController = require('../controllers/users')
 
+//route for get all contacts
 router.get('/', usersController.getAll);
+
+//route for get single contact
 router.get('/:id', usersController.getSingle);
 
+//route to Create a contact
 router.post('/', usersController.createContact);
 
+//route for update a contact
 router.put('/:id', usersController.updateSingleContact);
 
+//route for delete a contact
 router.delete('/:id', usersController.deleteContact);
 
 module.exports = router;
